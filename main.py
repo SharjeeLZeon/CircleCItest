b81eb26fd8bfcdd0796d3c31c3141b301705e811
SharjeeLZeon <muhammadsharjeel911@gmail.com>
2022-01-29 04:44:01 +0500

    updated

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return "Hello World"
+#@app.route('/')
+#def hello_world():
+#   return "Hello World"
 
-if __name__ == '__main__':
-   app.run()
+#if __name__ == '__main__':
+#   app.run() 
+
+print("Hellow world")
